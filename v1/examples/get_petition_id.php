1f937fbf476426657dc63cabb43aa6e76aa136c3
Eric Lukoff <elukoff@change.org>
2012-10-06 14:39:14 -0700

    Update the basic request example and script.

--- v1/examples/get_petition_id.php
+++ v1/examples/get_petition_id.php
@@ -1,25 +1,16 @@
 <?php
 
-$API_KEY = 'example';   // Omitted
-$SECRET = 'example';    // Omitted
+$API_KEY = 'example';
 
 $REQUEST_BASE_URL = 'https://api.change.org';
 $REQUEST_PATH = '/v1/petitions/get_id';
 
 $parameters = array(
-        'api_key' => $API_KEY,
-        'timestamp' => gmdate("Y-m-d\TH:i:s\Z"), // UTC time in ISO-8601 format
-        'petition_url' => 'http://www.change.org/petitions/dunkin-donuts-stop-using-styrofoam-cups-and-switch-to-a-more-eco-friendly-solution',
-        'endpoint' => $REQUEST_PATH
-    );
-
+  'api_key' => $API_KEY,
+  'petition_url' => 'http://www.change.org/petitions/dunkin-donuts-stop-using-styrofoam-cups-and-switch-to-a-more-eco-friendly-solution',
+);
 
 $query_string = http_build_query($parameters);
-$rsig = hash('sha256', $query_string . $SECRET);
-
-$parameters['rsig'] = $rsig;
-$query_string = http_build_query($parameters);
-
 $final_request_url = "$REQUEST_BASE_URL$REQUEST_PATH?$query_string";
 $response = file_get_contents($final_request_url);
 
